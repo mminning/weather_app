dc26682b545d11b3af1d455d441c9d532fe4dc49
mminning <minning.matthew@gmail.com>
2026-03-15 19:34:11 -0500

    working on getting weather data

--- weather_data.py
+++ weather_data.py
@@ -2,10 +2,10 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 import requests as rq
 
-
+# GET COORDINATES FROM CITY, STATE
 def get_coordinates(city, state):
-    # Initialize Nominatim API with a descriptive user_agent
-    # A user_agent is required by Nominatim's usage policy
+    """ gets coordinates for a city and a state.
+    Args: city (string), state (string, 2 letter ALL CAPS abbreviation)"""
     geolocator = Nominatim(user_agent="basshog")
 
     try:
@@ -22,22 +22,24 @@ def get_coordinates(city, state):
         # Handle cases where the geocoding service times out
         return get_coordinates(city, state)
 
-
-# Usage
-city = "Wichita"
-state = "KS"
-coords = get_coordinates(city, state)
+#usage
+coords = get_coordinates(city="Wichita", state="KS")
 
 if coords:
     latitude = coords[0]
     longitude = coords[1]
-    print(f"Coordinates for {city}, {state}:")
-    print(f"Latitude: {coords[0]}")
-    print(f"Longitude: {coords[1]}")
+    #print(f"Coordinates for {city}, {state}:")
+    #print(f"Latitude: {coords[0]}")
+    #print(f"Longitude: {coords[1]}")
 else:
     print("Location not found.")
 
-getforecast = rq.get(f"https://api.weather.gov/points/{latitude},{longitude}")
-getforecast_content = getforecast.json()
+# GET WEATHERDATA FROM COORDS
+def get_cityforecast(latitude, longitude):
+    getforecast = rq.get(f"https://api.weather.gov/points/{latitude},{longitude}")
+    getforecast_content = getforecast.json()
+    forecast_url = getforecast_content["properties"]["forecast"]
+    return forecast_url
 
-print(getforecast)
+url = get_cityforecast(latitude, longitude)
+print(url)
